e4b947b973957de5772334e203b49e4dc957bb61
slavic0369 <slavic0369@gmail.com>
2026-02-13 23:56:09 -0800

    Changed "Board", "Card", and "Columns from functions to classes. Also upodated main.tsx code to work with the new classes so it would not break.

--- src/renderer/src/components/Card.tsx
+++ src/renderer/src/components/Card.tsx
@@ -1,7 +1,15 @@
-export default function MyCard() {
-    return (
-        <div>
-            My Card
-        </div>
-    )
+export default class MyCard {
+
+	constructor(card_name) {
+		this.card_name = card_name;
+	}
+	
+	static print_name() {
+		return (
+			<div>
+				My Card
+			</div>
+		)
+    }
+    
 }
