f00c45bf9b4aea1720304bbefcc524880101e643
sudoLuko <lukeolson986@gmail.com>
2026-02-19 20:50:09 -0800

    add initalizeSchema()

--- src/main/database.ts
+++ src/main/database.ts
@@ -9,8 +9,58 @@ class DatabaseConnection {
     private constructor(dbPath: string) {
         this.dbPath = dbPath
         this.db = new Database(dbPath)
+        this.db.pragma('foreign_keys = ON') 
+        this.initializeSchema()
     }
 
+    private initializeSchema() {
+
+        this.db.prepare(`
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            created_at INTEGER,
+            username TEXT,
+            password TEXT
+            )
+        `).run()
+
+        this.db.prepare(`
+            CREATE TABLE IF NOT EXISTS boards (
+            id INTEGER PRIMARY KEY,
+            created_at INTEGER,
+            title TEXT,
+            description TEXT,
+            userID INTEGER,
+            FOREIGN KEY (userID) REFERENCES users(id)
+            )
+        `).run()
+
+        this.db.prepare(`
+            CREATE TABLE IF NOT EXISTS columns (
+            id INTEGER PRIMARY KEY,
+            created_at INTEGER,
+            title TEXT, 
+            position INTEGER,
+            boardID INTEGER,
+            FOREIGN KEY (boardID) REFERENCES boards(id)
+            )
+        `).run()
+
+        this.db.prepare(`
+            CREATE TABLE IF NOT EXISTS cards (
+            id INTEGER PRIMARY KEY,
+            created_at INTEGER,
+            title TEXT, 
+            description TEXT,
+            position INTEGER,
+            columnID INTEGER,
+            FOREIGN KEY (columnID) REFERENCES columns(id)
+            )
+        `).run()
+
+    }
+
+    
     public static getInstance(dbPath: string) {
         if ( DatabaseConnection.instance == null ) {
             DatabaseConnection.instance = new DatabaseConnection(dbPath)
@@ -26,6 +76,21 @@ class DatabaseConnection {
         return this.db.prepare(SQL).run(params)
     }
 
+    public beginTransaction() {
+        this.db.prepare('BEGIN').run()
+    }
+
+    public commit() {
+        this.db.prepare('COMMIT').run()
+    }
+
+    public rollback() {
+        this.db.prepare('ROLLBACK').run()
+    }
+
+    public close() {
+        this.db.close()
+    }
 
 }
 
