55fdb93a19b99a6d86a21a821fb62b1c95067f66
slavic0369 <slavic0369@gmail.com>
2026-02-22 22:42:30 -0800

    Added some styling to make the program look nicer. Also added a prototype area to make experimenting with different styples easier.

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,6 +4,28 @@ import Board from './components/Board'
 import Column from './components/Column'
 import Card from './components/Card'
 
+//Constants for easier style prototyping
+const COLUMN_BORDER: string = "3px solid #ccc";
+const COLUMN_TEXT_COLOR: string = "black";
+const COLUMN_FONT_WEIGHT: string = "bold";
+const COLUMN_BACKGROUND_COLOR: string = "white";
+const COLUMN_FONT_STYLE: string = "normal";
+
+//single size to keep both button and column text the same size
+const COLUMN_BUTTON_SIZE: string = "200%";
+const COLUMN_FONT_SIZE: string = COLUMN_BUTTON_SIZE;
+const BUTTON_FONT_SIZE: string = COLUMN_BUTTON_SIZE;
+
+//single size to keep padding consistant
+const COLUMN_PADDING: string = "10px";
+const COLUMN_PADDING_LEFT: string = COLUMN_PADDING;
+const COLUMN_PADDING_RIGHT: string = COLUMN_PADDING;
+const COLUMN_PADDING_TOP: string = COLUMN_PADDING;
+const COLUMN_PADDING_BOTTOM: string = COLUMN_PADDING;
+
+//Cards
+
+
 type MainViewProps = {
   board: Board
   selectedCol: Column
@@ -57,14 +79,14 @@ class MainView extends React.Component<MainViewProps, DisplayColProp> {
   render() {
     // convert to JSX
     const coolCol = this.state.cols.map((id) => (
-      <div key={id} style={{ border: "1px solid #ccc"}}>
+      <div key={id} style={{ border: COLUMN_BORDER, color: COLUMN_TEXT_COLOR, fontWeight: COLUMN_FONT_WEIGHT, backgroundColor: COLUMN_BACKGROUND_COLOR, fontSize: COLUMN_FONT_SIZE, paddingLeft: COLUMN_PADDING_LEFT, paddingRight: COLUMN_PADDING_RIGHT }}>
         My Column #{id}
       </div>
     ))
 
     return (
-      <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-        <button onClick={this.addCol}>+</button>
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center",  }}>
+        <button style= {{fontSize: BUTTON_FONT_SIZE}} onClick={this.addCol}>+</button>
         {coolCol}
       </div>
     )
@@ -76,3 +98,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <MainView board={new Board("board", [])} selectedCol={new Column("mainColumn", 0, [])} selectedCard={new Card(0, "", "starterCard")}/>
   </React.StrictMode>
 )
+
