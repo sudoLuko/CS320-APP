f91dd6ee87aa801dfb8033ce69cc6b080ae0eca1
Vyacheslav Lukiyanchuk <slavic0369@gmail.com>
2026-03-29 23:23:22 -0700

    Created and began work on accounts object to use in Electron

---
+++ src/renderer/src/components/Accounts.tsx
@@ -0,0 +1,15 @@
+export default class Accounts {
+	
+	firstNamee: string
+  	lastName: string
+  	userName: string
+  	password: string
+
+	constructor(firstNamee: string, lastName: string, userName: string, password: string) {
+		this.firstNamee = firstNamee;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.password = password;
+	}
+
+}
