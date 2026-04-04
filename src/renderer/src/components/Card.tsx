9291f2f017e9cbb1bdf8e609b8f0f9a8306867d9
dima <dmytro1108@gmail.com>
2026-04-03 22:34:50 -0700

    fix card render

--- src/renderer/src/components/Card.tsx
+++ src/renderer/src/components/Card.tsx
@@ -1,9 +1,11 @@
 export default class MyCard {
+	card_id: number
 	card_name: number
   	title: string
   	description: string
 
-	constructor(card_name: number, title: string, description: string) {
+	constructor(card_id: number, card_name: number, title: string, description: string) {
+		this.card_id = card_id;
 		this.card_name = card_name;
 		this.title = title;
 		this.description = description;
